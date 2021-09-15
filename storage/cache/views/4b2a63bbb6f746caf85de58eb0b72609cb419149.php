b5ba4c2bf7649ff0a5153058409c89d2ca1a0e92
Iván López Ordorica <52330974+wbrak@users.noreply.github.com>
2021-09-15 20:07:29 +0200

    Delete storage directory

--- storage/cache/views/4b2a63bbb6f746caf85de58eb0b72609cb419149.php
+++
@@ -1,9 +0,0 @@
-<?php $__env->startSection('content'); ?>
-    <div style="padding: 15px;">
-        <h2>
-            Dashboard Page <?php echo e(env('NON_EXISTING_ENV_VALUE', 'Dashboard Home Page')); ?>
-
-        </h2>
-    </div>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layouts/app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /opt/lampp/htdocs/izar-framework/resources/views/dashboard/home.blade.php ENDPATH**/ ?>
--- storage/cache/views/501e07a64a19104fca6bbe7ca351befed211fa31.php
+++
@@ -1,7 +0,0 @@
-<?php $__env->startSection('content'); ?>
-    <register>
-
-    </register>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /opt/lampp/htdocs/izar-framework/resources/views/auth/register.blade.php ENDPATH**/ ?>
